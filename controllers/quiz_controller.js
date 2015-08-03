5c09cf9339162b7cbe82eae1a813b3ed8eaf49f0
gabrielarranz <gabriel.arranz.fdez@gmail.com>
2015-08-03 20:34:34 +0200

    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -66,16 +66,19 @@ exports.new = function(req, res) {
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-  quiz.validate().then (
+  quiz
+  .validate()
+  .then(
     function(err){
       if (err) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
-        // save: guarda en DB campos pregunta y respuesta de quiz
-        quiz .save({fields: ["pregunta", "respuesta"]}).then( function(){ res.redirect('/quizes')})
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')})
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
-  );
+  ).catch(function(error){next(error)});
 };
 
 // GET /quizes/:id/edit
@@ -102,5 +105,12 @@ exports.update = function(req, res) {
         .then( function(){ res.redirect('/quizes');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
-  );
+  ).catch(function(error){next(error)});
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
 };
