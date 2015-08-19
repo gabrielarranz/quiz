42e8be2dc37fc2906714c0e5f5b7499fea7687ea
gabrielarranz <gabriel.arranz.fdez@gmail.com>
2015-08-19 18:16:41 +0200

    Auto Logout

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -33,6 +33,7 @@ exports.create = function(req, res) {
         // Crear req.session.user y guardar campos   id  y  username
         // La sesión se define por la existencia de:    req.session.user
         req.session.user = {id:user.id, username:user.username};
+        req.session.cookie.expires = 120000
 
         res.redirect(req.session.redir.toString());// redirección a path anterior a login
     });
